1130fe4582cab1628ac094ae9eb5742399616eb9
Leonardo Torres <leonardot.trab@gmail.com>
2019-03-24 18:12:37 -0300

    Refactoring to use controllers

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,93 +1,16 @@
 const express = require('express');
 const multer = require("multer");
-const sharp = require('sharp');
-const User = require("../models/user");
 const auth = require("../middleware/auth");
-const { sendWelcomeEmail, sendDeleteEmail } = require('../emails/account');
+const UserController = require("../controllers/UserController");
 const router = new express.Router();
 
-
-router.post('/users', async (req, res) => {
-    const user = new User(req.body);
-
-    try {
-        await user.save();
-        sendWelcomeEmail(user.email, user.name);
-        const token = await user.generateAuthToken();
-        res.status(201).send({ user, token });
-    } catch (e) {
-        res.status(400).send(e);
-    }
-});
-
-router.post("/users/login", async (req, res) => {
-    try {
-        const user = await User.findByCredentials(req.body.email, req.body.password);
-        const token = await user.generateAuthToken();
-        res.send({ user, token });
-    } catch (e) {
-        res.status(400).send();
-    }
-});
-
-router.post("/users/logout", auth, async (req, res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter(token => token.token !== req.token);
-        await req.user.save();
-        res.send()
-    } catch (e) {
-        res.status(500).send();
-    }
-});
-
-
-router.post("/users/logout/all", auth, async (req, res) => {
-    try {
-        req.user.tokens = [];
-        await req.user.save();
-        res.send()
-    } catch (e) {
-        res.status(500).send();
-    }
-});
-
-
-router.get("/users/me", auth, async (req, res) => {
-    res.send(req.user);
-});
-
-router.patch("/users/me", auth, async (req, res) => {
-    const updates = Object.keys(req.body);
-    const allowedUpdates = ['name', 'email', 'password', 'age'];
-
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
-
-    if (!isValidOperation) {
-        return res.status.send({ error: "Invalid updates!" });
-    }
-
-    try {
-
-        updates.forEach((update) => req.user[update] = req.body[update])
-        await req.user.save()
-
-        res.send(req.user);
-    } catch (e) {
-        res.status(400).send();
-    }
-});
-
-
-router.delete("/users/me", auth, async (req, res) => {
-    try {
-        // const user = await User.findByIdAndDelete(req.user._id);
-        await req.user.remove();
-        sendDeleteEmail(req.user.email, req.user.name);
-        res.send(req.user);
-    } catch (e) {
-        res.status(500).send();
-    }
-});
+router.post('/users', UserController.createUser);
+router.post("/users/login", UserController.loginUser);
+router.post("/users/logout", auth, UserController.logoutUser);
+router.post("/users/logout/all", auth, UserController.logoutAll);
+router.get("/users/me", auth, UserController.readProfile);
+router.patch("/users/me", auth, UserController.updateUser);
+router.delete("/users/me", auth, UserController.deleteUser);
 
 const upload = multer({
     limits: {
@@ -101,35 +24,9 @@ const upload = multer({
         callback(undefined, true);
     }
 })
-
 // Add Avatar to user
-router.post("/users/me/avatar", auth, upload.single('avatar'), async (req, res) => {
-    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer();
-    req.user.avatar = buffer;
-    await req.user.save();
-    res.send();
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message });
-});
-
-router.delete("/users/me/avatar", auth, async (req, res) => {
-    req.user.avatar = undefined;
-    await req.user.save();
-    res.send();
-});
-
-router.get("/users/:id/avatar", async (req, res) => {
-    try {
-        const user = await User.findById({ _id: req.params.id });
-        if (!user || !user.avatar) {
-            throw new Error();
-        }
-
-        res.set('Content-Type', 'image/png');
-        res.send(user.avatar);
-    } catch (e) {
-        res.status(404).send();
-    }
-});
+router.post("/users/me/avatar", auth, upload.single('avatar'), UserController.uploadAvatar, UserController.uploadError);
+router.delete("/users/me/avatar", auth, UserController.deleteAvatar);
+router.get("/users/:id/avatar", UserController.getAvatar);
 
 module.exports = router;
